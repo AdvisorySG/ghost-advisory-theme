cac9e94ae9c009e0edd00d24a36e2494ff4c3a0a
Ng Wei En <weien1292@gmail.com>
2020-08-30 22:00:56 +0800

    Post: Implement basic prototype (#63)
    Co-authored-by: partysnax <pat9120219@gmail.com>
    Co-authored-by: unknown <appledcse@yahoo.com>
    Co-authored-by: Hui Lin <huilyn.thl@gmail.com>

---
+++ assets/js/toggledropdown.js
@@ -0,0 +1,50 @@
+/* an experimental widget that rewrites h1s into toggle dropdowns */
+
+$(function () {
+    var isHiding = {};
+
+    $(".gh-content > h1").each(function () {
+        $(this).addClass("h1-toggle");
+    });
+
+    $(".h1-toggle").each(function () {
+        var $header = $(this);
+        var headerId = $header.attr("id");
+
+        function hideHeader() {
+            $header.addClass("h1-toggle-hide");
+            $header.removeClass("h1-toggle-show");
+            $header.nextUntil("h1").hide();
+
+            $("#" + headerId + " > i").remove();
+            $header.prepend('<i class="fas fa-chevron-up h1-toggle-icon"></i>');
+        }
+
+        function showHeader() {
+            $header.addClass("h1-toggle-show");
+            $header.removeClass("h1-toggle-hide");
+            $header.nextUntil("h1").show();
+
+            $("#" + headerId + " > i").remove();
+            $header.prepend(
+                '<i class="fas fa-chevron-down h1-toggle-icon"></i>'
+            );
+        }
+
+        // set headers to be hidden by default,
+        // unless the header has been set to show...
+        if ($header.hasClass("h1-toggle-show")) {
+            showHeader($header);
+            isHiding[headerId] = false;
+        } else {
+            hideHeader($header);
+            isHiding[headerId] = true;
+        }
+        // ...and to toggle between hidden/shown when clicked.
+        $header.click(function () {
+            if (isHiding[headerId]) showHeader();
+            else hideHeader();
+            isHiding[headerId] = !isHiding[headerId];
+        });
+    });
+});
