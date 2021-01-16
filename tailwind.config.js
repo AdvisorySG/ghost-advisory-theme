4bce01dcface2fb3efe191310161b95cb8da232a
Ng Wei En <weien1292@gmail.com>
2021-01-16 15:18:10 +0800

    Tailwind: Initial setup (#136)
    Co-authored-by: Benjamin Tan <benjamin@dev.ofcr.se>

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./*.hbs", "./**/*.hbs"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
