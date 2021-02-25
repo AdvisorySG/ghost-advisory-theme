718de25019acd564d443e7f03b3b780a05587412
partysnax <pat9120219@gmail.com>
2021-02-25 13:53:42 +0800

    Implement event card date system

---
+++ assets/js/eventCardDateFormatter.js
@@ -0,0 +1,12 @@
+$(function(){
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+
+    $(".event-card").each(function(){
+        var dateArray = $(this).find(".event-date-month").eq(0).text().split('-');
+        $(this).find(".event-date-day").eq(0).text(dateArray[2]);
+        $(this).find(".event-date-month").eq(0).text(monthNames[parseInt(dateArray[1])-1]);
+    });
+
+});
