45de05f112c98ef2c6eb0a356a5620441acbc90e
Ng Wei En <weien1292@gmail.com>
2021-07-31 13:43:30 +0800

    page-stories: Add a rudimentary Load More button

--- assets/js/storiesFilter.js
+++ assets/js/storiesFilter.js
@@ -1,3 +1,5 @@
+const INCREMENT = 2;
+
 const retrieveFilters = (target) => {
     const filterOptions = $(target).val() || [];
     const isInternalTag = $(target).hasClass("internal-tag");
@@ -5,25 +7,52 @@ const retrieveFilters = (target) => {
     return filterOptions.map((value) => tagPrefix + value);
 };
 
-const filterPosts = () => {
+const filterPosts = (limit) => {
     const primaryFilters = retrieveFilters($("#primary-filter"));
     const internalFilters = retrieveFilters($("#internal-filter"));
     const filters = primaryFilters.flatMap((filter1) =>
         internalFilters.map((filter2) => filter1 + filter2)
     );
 
+    let count = 0;
+    let isMoreLeft = true;
     $(".gh-postfeed")
         .children()
         .each(function () {
+            if (count >= limit) {
+                $(this).addClass("d-none");
+                isMoreLeft = false;
+                return;
+            }
+
             if (filters.some((filter) => $(this).is(filter))) {
                 $(this).removeClass("d-none");
+                count += 1;
             } else {
                 $(this).addClass("d-none");
             }
         });
+
+    if (isMoreLeft) {
+        $("#load-more").addClass("d-none");
+        $("#load-more-text").removeClass("d-none");
+    } else {
+        $("#load-more").removeClass("d-none");
+        $("#load-more-text").addClass("d-none");
+    }
 };
 
 $(document).ready(() => {
-    filterPosts();
-    $("#primary-filter, #internal-filter").change(() => filterPosts());
+    let postLimit = INCREMENT;
+    filterPosts(postLimit);
+
+    $("#primary-filter, #internal-filter").change(() => {
+        postLimit = INCREMENT;
+        filterPosts(postLimit);
+    });
+
+    $("#load-more").click(() => {
+        postLimit += INCREMENT;
+        filterPosts(postLimit);
+    });
 });
