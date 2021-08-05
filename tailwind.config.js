1e36dd5822d31bdc95d15185a07ea1bbaae42048
Eykf1997 <76686621+Eykf1997@users.noreply.github.com>
2021-08-05 17:11:26 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,46 +8,7 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        width: {
-            '200': '200px',
-            '300': '300px',
-            '400': '400px',
-            '500': '500px',
-            '600': '600px',
-            '700': '700px',
-            '800': '800px',
-            '900': '900px',
-            '1000': '1000px',
-            
-           },
-           height: {
-            '200': '200px',
-            '300': '300px',
-            '400': '400px',
-            '500': '500px',
-            '600': '600px',
-            '700': '700px',
-            '800': '800px',
-            '900': '900px',
-            '1000': '1000px',
-            
-           },
-           screens: {
-            'sm': '200px',
-            // => @media (min-width: 640px) { ... }
-      
-            'md': '768px',
-            // => @media (min-width: 768px) { ... }
-      
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-      
-            'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
-      
-            '2xl': '1536px',
-            // => @media (min-width: 1536px) { ... }
-          },
+        extend: {},
     },
     variants: {
         extend: {},
