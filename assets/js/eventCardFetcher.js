ec37f627704daad47b15ba3398c1490f9a532284
Terence Chan Zun Mun <zunmun@gmail.com>
2021-09-11 12:41:33 +0800

    Use Alpine JS for Templating Event Cards (#306)
    This commit replaces Handlebars with Alpine JS, which is more lightweight and quite likely to be functionally adequate for the majority of our use cases. This considerably simplifies our current build pipeline and eliminates any confusion from having files written in the Ghost-variant Handlebars and vanilla Handlebars.
    
    As part of this commit, commit df51c8dbe2bc019258237e24d0747036c6fb593f has been reverted.
    
    Co-authored-by: Ng Wei En <weien1292@gmail.com>

--- assets/js/eventCardFetcher.js
+++ assets/js/eventCardFetcher.js
@@ -1,59 +1,65 @@
-const eventCard = require("./partials/event-card.hbs");
+const EVENTS_LIMIT = 3;
 
-$(function () {
-    const api = new GhostContentAPI({
-        url: ADVISORY.SITE_URL,
-        key: ADVISORY.CONTENT_API_KEY,
-        version: "v4",
-    });
-
-    const MONTHS = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-
-    const parseEvent = (post) => {
-        const splitTitle = post.title.split(":"); // <!> This assumes all event titles contain a colon
-        const splitDate = post.custom_excerpt.split("-");
-        return {
-            day: splitDate[2],
-            month: MONTHS[parseInt(splitDate[1]) - 1],
-            titlePrefix: splitTitle[0],
-            titleSuffix: splitTitle[1],
-        };
-    };
-
-    const renderContainer = (posts) => {
-        if (posts.length > 0) {
-            $("#event-card-container").html(
-                eventCard({
-                    events: posts.slice(0, 4).map(parseEvent),
-                })
-            );
-        } else {
-            $("#event-card-placeholder").html("No upcoming events.");
-        }
-    };
-
-    // Fetching event posts from API
-    const date = new Date().toISOString().split("T")[0];
-    const filterQuery = `tag:hash-event+custom_excerpt:>='${date}'`;
-    api.posts
-        .browse({
-            include: "tags",
-            filter: filterQuery,
-            order: "custom_excerpt ASC",
-        })
-        .then(renderContainer)
-        .catch(console.error);
+const api = new GhostContentAPI({
+    url: ADVISORY.SITE_URL,
+    key: ADVISORY.CONTENT_API_KEY,
+    version: "v4",
 });
+
+const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const parseEvent = (post) => {
+    const splitTitle = post.title.split(":"); // <!> This assumes all event titles contain a colon
+    const splitDate = post.custom_excerpt.split("-");
+    return {
+        day: splitDate[2],
+        month: MONTHS[parseInt(splitDate[1]) - 1],
+        titlePrefix: splitTitle[0],
+        titleSuffix: splitTitle[1],
+    };
+};
+
+const date = new Date().toISOString().split("T")[0];
+
+window.eventCards = function () {
+    return {
+        posts: [],
+        isShowPlaceholder: true,
+        placeholderText: null,
+        init() {
+            let referenceObj = this;
+            const filterQuery = `tag:hash-event+custom_excerpt:>='${date}'`;
+            api.posts
+                .browse({
+                    include: "tags",
+                    filter: filterQuery,
+                    order: "custom_excerpt ASC",
+                })
+                .then((posts) => {
+                    if (posts.length > 0) {
+                        referenceObj.isShowPlaceholder = false;
+                        referenceObj.posts = posts
+                            .slice(0, EVENTS_LIMIT + 1)
+                            .map(parseEvent);
+                    } else {
+                        referenceObj.isShowPlaceholder = true;
+                        referenceObj.placeholderText = "No upcoming events.";
+                    }
+                })
+                .catch(console.error);
+        },
+    };
+};
